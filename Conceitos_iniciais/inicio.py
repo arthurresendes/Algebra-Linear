ab7e98ce13be15aab1b1948c71fef282cadcde77
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-15 20:05:36 -0300

    Atualizar conceitos

---
+++ Conceitos_iniciais/inicio.py
@@ -0,0 +1,17 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import pandas as pd
+import sentence_transformers
+from sentence_transformers import SentenceTransformer
+import tensorflow as tf
+import torch
+
+print(matplotlib.__version__)
+print(np.__version__)
+print(pd.__version__)
+print(sentence_transformers.__version__)
+print(torch.__version__)
+print(tf.__version__)
+
