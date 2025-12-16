52398a37deded3930a374350d299e458d4e5936e
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-16 17:50:47 -0300

    Sistema Linear

---
+++ Conceitos_iniciais/Revisao/sistema_linear.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x1_r1 = np.array([0,10,20,30,40,50,60])
+x1_r2 = np.array([0,10,20,30,40,50,60])
+
+x2_r1 = 127.35 - x1_r1
+x2_r2 = 229.24 - 2 * x1_r2
+
+print(x2_r1)
+print(x2_r2)
+
+plt.plot(x1_r1,x2_r1 , label="casal_1")
+plt.plot(x1_r2,x2_r2,label = "casal_2")
+plt.legend()
+plt.savefig('sem solucao.png')
+plt.show()
+
+x1_r3 = np.array([0,10,20,30,40,50])
+x2_r3 = 134.85 - 2 * x1_r3
+print(x2_r3)
+
+plt.plot(x1_r1,x2_r1 , label="casal_1")
+plt.plot(x1_r3,x2_r3,label = "casal_2")
+plt.legend()
+plt.savefig('se cruzam.png')
+plt.show()
