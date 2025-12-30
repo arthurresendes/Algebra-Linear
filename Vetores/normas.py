e80196d04edd42650a0fe8c7fff91b2326a3bf9b
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-30 16:48:23 -0300

    Vetores unitarios e de base

--- Vetores/normas.py
+++ Vetores/normas.py
@@ -1,5 +1,5 @@
 import numpy as np
 
 vetor = np.array([7.5,8.25])
-print(f"Norma 1 (distancia manhattan): {np.linalg.norm(vetor,ord=1)}")
-print(f"Norma 2 (distancia euclediana): {np.linalg.norm(vetor,ord=2)}")
+print(f"Norma 1 (distancia manhattan - soma das coordenadas): {np.linalg.norm(vetor,ord=1)}")
+print(f"Norma 2 (distancia euclediana - comprimento do vetor): {np.linalg.norm(vetor,ord=2)}")
--- Vetores/vetor_unitario_base.py
+++ Vetores/vetor_unitario_base.py
@@ -2,13 +2,11 @@ import numpy as np
 
 vetor = np.array([7.5,8.25])
 print(np.linalg.norm(vetor))
-
 normalizacao_vetor = vetor/np.linalg.norm(vetor)
 print(normalizacao_vetor)
 print(np.linalg.norm(normalizacao_vetor))
 
 vetor_i = np.array([1,0])
 vetor_j = np.array([0,1])
-
 calculo = vetor[0] * vetor_i + vetor[1] * vetor_j
 print(calculo)
