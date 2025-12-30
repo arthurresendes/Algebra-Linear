3e8780f094fe5d9903129ed7ed8509a5213edb34
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-30 15:27:42 -0300

    Plotagem

--- Vetores/plotagem.py
+++ Vetores/plotagem.py
@@ -1,1 +1,26 @@
-print("teste")
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import sklearn
+
+
+vetor = np.array([7.5,8.25])
+print(f"Valores do vetor: {vetor}")
+print(f"Valor do tipo: {vetor.ndim}")
+print(f"Quntas linhas e colunas: {vetor.shape}")
+print(f"Quantos elementos: {vetor.size}")
+
+# plt.scatter(x=vetor[0],y=vetor[1])
+plt.scatter(*vetor)
+plt.show()
+
+plt.scatter(*vetor)
+plt.xlim(xmin=0)
+plt.ylim(ymin=0)
+plt.show()
+
+plt.scatter(*vetor)
+plt.xlim(xmin=0)
+plt.ylim(ymin=0)
+plt.arrow(0,0,*vetor,length_includes_head = True, head_width=0.25)
+plt.show()
