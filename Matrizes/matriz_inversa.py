33bd3bf5bab7f72265fc03bfe89836af282d707e
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-05 18:10:19 -0300

    Conceitos matriizes inversa

---
+++ Matrizes/matriz_inversa.py
@@ -0,0 +1,13 @@
+import numpy as np
+import scipy
+
+matriz_simetrica = np.array([[1, 3, 2, 5],
+     [3, 2, 8, 1],
+     [2, 8, 6, 7],
+     [5, 1, 7, 4]])
+
+
+simetrica_matriz_inversa = np.linalg.inv(matriz_simetrica)
+print(simetrica_matriz_inversa)
+print(np.dot(matriz_simetrica,simetrica_matriz_inversa))
+print(np.allclose(np.dot(matriz_simetrica,simetrica_matriz_inversa), np.identity(4)))
