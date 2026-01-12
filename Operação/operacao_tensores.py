5ffc78500d9b630718c9a6f1d17e8ff1f40e2b9d
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-12 14:23:30 -0300

    Operações entre tensores

---
+++ Operação/operacao_tensores.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+print("Operação entre tensores")
+array1 =  np.array([2,1,0])
+array2 =  np.array([3,4,5])
+print(np.dot(array1,array2))
+print("Comprimento: ")
+print(np.dot(array1,array1))
