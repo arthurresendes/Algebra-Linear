b0bcd75dd983dd10b8deac1162215cafedfee82a
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-20 10:29:15 -0300

    Clean Code

--- Transformação/matriz_cisalhamento_rotacao.py
+++ Transformação/matriz_cisalhamento_rotacao.py
@@ -1,19 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-
-def plot_vectors(arrays, transformation_matrix):
-    plt.figure(figsize=(5, 5))
-    plt.axhline(y=0, linestyle='--')
-    plt.axvline(x=0, linestyle='--')
-    plt.xticks(np.arange(-10, 10, 0.5))
-    plt.yticks(np.arange(-10, 10, 0.5))
-    color_palete = [('blue', 'orange'), ('green', 'brown')]
-    for i, array in enumerate(arrays):
-        array_transformed = np.matmul(transformation_matrix, array)
-        plt.arrow(0, 0, *array, length_includes_head=True, head_width=0.05, color=color_palete[i][0], label=f'array_{i+1}')
-        plt.arrow(0, 0, *array_transformed, length_includes_head=True, head_width=0.05, color=color_palete[i][1], label=f'array_{i+1}_transformed')
-    plt.show()
+from .plotagem import plot_vectors
 
 array_1 = np.array([1, 2])
 array_2 = np.array([3, 4])
@@ -21,13 +7,13 @@ array_2 = np.array([3, 4])
 D = np.array([[1,0],[0,0.25]])
 plot_vectors([array_1],D)
 
-
+# Rotacionando 10 graus
 degrees = 10
 E = np.array([[np.cos(np.deg2rad(degrees)), -np.sin(np.deg2rad(degrees))],
               [np.sin(np.deg2rad(degrees)),  np.cos(np.deg2rad(degrees))]])
 print(E)
 
 array_1_transformed = np.matmul(E, array_1)
-print(array_1_transformed)
+print(f"Rotação em 10 graus: {array_1_transformed}")
 
 plot_vectors([array_1], E)
--- Transformação/matriz_determinante.py
+++ Transformação/matriz_determinante.py
@@ -1,19 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-
-def plot_vectors(arrays, transformation_matrix):
-    plt.figure(figsize=(5, 5))
-    plt.axhline(y=0, linestyle='--')
-    plt.axvline(x=0, linestyle='--')
-    plt.xticks(np.arange(-10, 10, 0.5))
-    plt.yticks(np.arange(-10, 10, 0.5))
-    color_palete = [('blue', 'orange'), ('green', 'brown')]
-    for i, array in enumerate(arrays):
-        array_transformed = np.matmul(transformation_matrix, array)
-        plt.arrow(0, 0, *array, length_includes_head=True, head_width=0.05, color=color_palete[i][0], label=f'array_{i+1}')
-        plt.arrow(0, 0, *array_transformed, length_includes_head=True, head_width=0.05, color=color_palete[i][1], label=f'array_{i+1}_transformed')
-    plt.show()
+from .plotagem import plot_vectors
 
 array_1 = [3, 0]
 array_2 = [0, 3]
