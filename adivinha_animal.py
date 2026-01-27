a6bb415d132f96494e1c42bdf70ad958f65943da
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-26 22:55:16 -0300

    Adivinha animal

---
+++ adivinha_animal.py
@@ -0,0 +1,29 @@
+from sklearn.svm import LinearSVC
+import numpy as np
+
+
+gato1 = np.array([1,0,2])
+gato2 = np.array([1,0,0])
+
+dog1 = np.array([1,1,1])
+dog2 = np.array([1,2,2])
+
+galo1 = np.array([0,1,0])
+galo2 = np.array([0,0,0])
+
+treino_x = [gato1,gato2,dog1,dog2,galo1,galo2]
+treino_y = [0,0,1,1,2,2]
+
+modelo = LinearSVC()
+modelo.fit(treino_x,treino_y)
+
+res = modelo.predict([np.array([1,0,2])])
+
+if res == 0:
+    print("Gato")
+elif res ==1:
+    print("Cachorro")
+elif res == 2:
+    print("Galo")
+else:
+    print("Nao identificado")
