7dc0357e9cf4a328ca3902e74b6e6e760f0f50e2
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-02-23 14:52:23 -0300

    Criar sistema3x3_manual.py

---
+++ Faculdade/sistema3x3_manual.py
@@ -0,0 +1,22 @@
+''
+primeiraEQ = x + y + z = 6
+segundaaEQ = x - y + z = 2
+terceiraEQ = 2x + y - z = 1
+
+primeira_terceira = 3x + 2y = 7
+segunda_terceira = 3x = 3
+'''
+
+x = 3/3 # x = 1
+
+'''
+primeira_terceira = 2y = 7-3
+primeira_terceira = 2y = 4
+primeira_terceira = y = 4/2
+'''
+
+y = 4/2
+
+z = 6 - x- y
+
+print(f"{x,y,z}")
