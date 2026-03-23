23304617994c5169b8470959d0f9264b89a69f3b
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-03-23 15:24:13 -0300

    Espaço vetorial

--- Faculdade/Matrizes/espaco_vetorial.py
+++ Faculdade/Matrizes/espaco_vetorial.py
@@ -6,5 +6,8 @@ v2 = np.array([4, 5, 6])
 
 # Operações básicas
 soma = v1 + v2          # Adição vetorial: [5, 7, 9]
+print(soma)
 escalar = 2 * v1       # Multiplicação por escalar: [2, 4, 6]
+print(escalar)
 produto_p = np.dot(v1, v2) # Produto escalar: 1*4 + 2*5 + 3*6 = 32
+print(produto_p)
